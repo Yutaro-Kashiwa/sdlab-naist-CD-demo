f05fcff03b58138d2e8d8cd72be0da00965efe85
Afnan <90241150+Afnanaz@users.noreply.github.com>
2025-12-28 08:02:23 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -25,6 +25,11 @@ export default function Home() {
            <td>柏 祐太郎</td>
            <td>2025/2/25</td>
          </tr>
+         </tr>
+         <tr>
+           <td>Afnan Sukri Anwar Abdullah</td>
+           <td>2025/2/28</td>
+         </tr>
        </table>      
 </main>
       <Footer />
